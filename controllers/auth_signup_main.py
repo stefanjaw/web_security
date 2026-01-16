c51789b5e4a04df18f1cb3b8dd401ca633b8fee6
Stefan J <stefanj@avalantec.com>
2026-01-16 00:36:15 +0000

    Updated

--- controllers/auth_signup_main.py
+++ controllers/auth_signup_main.py
@@ -4,6 +4,8 @@ from odoo.http import request
 
 from odoo.addons.web.controllers.home import ensure_db, Home, SIGN_UP_REQUEST_PARAMS, LOGIN_SUCCESSFUL_PARAMS
 
+from markupsafe import Markup
+
 import logging
 _logging = _logger = logging.getLogger(__name__)
 
@@ -12,13 +14,16 @@ class AuthSignupHomeInherited(AuthSignupHome):
     @http.route('/web/signup', type='http', auth='public', website=True, sitemap=False)
     def web_auth_signup(self, *args, **kw):
         login = kw.get('login')
+
+        self._set_template_body_html_with_token()
+
         response = super().web_auth_signup(*args, **kw)
 
         user_id = request.env['res.users'].search([
                 ('login', '=', login)
             ])
         signup_token = request.session.session_token
-        if len(user_id) == 1 and signup_token not in [None,False,""]:
+        if len(user_id) == 1 and signup_token in [None,False,""]:
             user_id.sudo().partner_id.signup_token = signup_token
 
         if len(user_id) == 1 and user_id.email_verified == False:
@@ -26,6 +31,27 @@ class AuthSignupHomeInherited(AuthSignupHome):
             return request.redirect('/web/session/logout?redirect=/web/check_email')
         return response
 
+    def _set_template_body_html_with_token(self):
+        template_id = request.env.ref('auth_signup.mail_template_user_signup_account_created', raise_if_not_found=False)
+        if len(template_id) == 1:
+            text_original = 'auth_login={{object.email}}" style='
+            text_new =      'auth_login={{object.email}}&token={{object.partner_id.signup_token}}" style='
+                        
+            template_body_html = template_id.sudo().body_html
+            template_body_html_str = str(template_body_html)
+            
+            if text_original in template_body_html_str:
+                _logger.info(f"    ==== In Mail Template |{template_id.name}| replace\n\tFROM: |{text_original}|\n\tTO: |{text_new}|")
+                template_body_html_str = template_body_html_str.replace(
+                        text_original, text_new
+                    )
+                body_html = Markup( template_body_html_str )
+
+                template_id.sudo().write({
+                        'body_html': Markup( template_body_html_str )
+                    })
+        return
+    
     def web_auth_reset_password(self, *args, **kw):
         kw_token = kw.get('token')
         kw_login = kw.get('login')
